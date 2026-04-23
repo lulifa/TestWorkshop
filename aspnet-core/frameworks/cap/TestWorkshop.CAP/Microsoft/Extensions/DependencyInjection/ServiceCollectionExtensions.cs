c716d15975c7b34d6554a3caa401d96b8295c97a
lulifa <814570123@qq.com>
2026-04-23 14:43:52 +0800

    feat：新增cap相关的封装实现

---
+++ aspnet-core/frameworks/cap/TestWorkshop.CAP/Microsoft/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// CAP ServiceCollectionExtensions
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds and configures the consistence services for the consistency.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="capAction"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddCAPEventBus(this IServiceCollection services, Action<CapOptions> capAction)
+    {
+        services.AddCap(capAction);
+        // 替换为自己的实现
+        services.AddSingleton<ISubscribeInvoker, AbpCAPSubscribeInvoker>();
+        services.AddSingleton<ISerializer, AbpCapSerializer>();
+
+        // 移除默认CAP启动接口
+        services.RemoveAll(service =>
+        {
+            if (service.ServiceType.IsAssignableFrom(typeof(IBootstrapper)))
+            {
+                return true;
+            }
+            // 默认Bootstrapper
+            if (service.ImplementationType != null &&
+                service.ImplementationType.IsAssignableTo(typeof(IBootstrapper)))
+            {
+                return true;
+            }
+            // 默认Bootstrapper HostService
+            if (service.ImplementationFactory != null &&
+                service.ImplementationFactory.Method.ReturnType.IsAssignableTo(typeof(IBootstrapper)))
+            {
+                return true;
+            }
+
+            return false;
+        });
+        // 使用重写的接口,不使用BackgroundService
+        services.AddSingleton<AbpCAPBootstrapper>();
+        services.AddSingleton<IBootstrapper>(sp => sp.GetRequiredService<AbpCAPBootstrapper>());
+
+        return services;
+    }
+}
---
+++ aspnet-core/frameworks/cap/TestWorkshop.CAP/TestWorkshop/CAP/IFailedThresholdCallbackNotifier.cs
@@ -0,0 +1,6 @@
+﻿namespace TestWorkshop.CAP;
+
+public interface IFailedThresholdCallbackNotifier
+{
+    Task NotifyAsync(AbpCAPExecutionFailedException exception);
+}
