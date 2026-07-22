e726d58118dcaa1f2817f96e522855b0e7b02a39
lulifa <814570123@qq.com>
2026-07-22 14:17:06 +0800

    feat：调整前端界面workbench

--- vben5/vue-vben-admin-5.6.0/apps/web-antd/src/views/dashboard/workspace/components/Workbench.vue
+++ vben5/vue-vben-admin-5.6.0/apps/web-antd/src/views/dashboard/workspace/components/Workbench.vue
@@ -35,14 +35,14 @@ const { getListApi: getFavoriteMenusApi, deleteApi: deleteFavoriteMenuApi } =
 const { uiFramework } = useAppConfig(import.meta.env, import.meta.env.PROD);
 
 const defaultMenus: FavoriteMenu[] = [
-  // {
-  //   id: '1',
-  //   color: '#1fdaca',
-  //   icon: 'ion:home-outline',
-  //   displayName: $t('workbench.content.favoriteMenu.home'),
-  //   path: '/',
-  //   isDefault: true,
-  // },
+  {
+    id: '1',
+    color: '#1fdaca',
+    icon: 'ion:home-outline',
+    displayName: $t('workbench.content.favoriteMenu.home'),
+    path: '/',
+    isDefault: true,
+  },
   // {
   //   id: '2',
   //   color: '#bf0c2c',
