61a8b9d5c79674b180e15b17310efa35a048dbde
lulifa <814570123@qq.com>
2026-07-28 17:48:09 +0800

    feat：优化ip显示相关方法

--- aspnet-core/services/TestWorkshop.HttpApi.Host/Extensions/CustomAuditLogInfoToAuditLogConverter.cs
+++ aspnet-core/services/TestWorkshop.HttpApi.Host/Extensions/CustomAuditLogInfoToAuditLogConverter.cs
@@ -1,6 +1,4 @@
-﻿using IPTools.Core;
-using UAParser;
-using Volo.Abp.AspNetCore.ExceptionHandling;
+﻿using Volo.Abp.AspNetCore.ExceptionHandling;
 using Volo.Abp.Auditing;
 using Volo.Abp.AuditLogging;
 using Volo.Abp.Guids;
@@ -25,47 +23,8 @@ public class CustomAuditLogInfoToAuditLogConverter : AuditLogInfoToAuditLogConve
 
     public override Task<AuditLog> ConvertAsync(AuditLogInfo auditLogInfo)
     {
-        auditLogInfo.ClientIpAddress = GetIpWithLocation(auditLogInfo.ClientIpAddress);
-        auditLogInfo.BrowserInfo = GetBrowserInfo(auditLogInfo.BrowserInfo);
+        auditLogInfo.ClientIpAddress = IpLocationHelper.GetIpWithLocation(auditLogInfo.ClientIpAddress);
+        auditLogInfo.BrowserInfo = IpLocationHelper.GetBrowserInfo(auditLogInfo.BrowserInfo);
         return base.ConvertAsync(auditLogInfo);
-    }
-
-    private string GetIpWithLocation(string ip)
-    {
-        if (string.IsNullOrEmpty(ip))
-            return ip;
-
-        // 处理本地地址
-        if (ip == "127.0.0.1" || ip == "::1" || ip == "0:0:0:0:0:0:0:1" || ip.Equals("localhost", StringComparison.OrdinalIgnoreCase))
-            return "127.0.0.1(本地)";
-
-        try
-        {
-            var info = IpTool.Search(ip);
-            return $"{info.IpAddress}({info.Province}{info.City})";
-        }
-        catch
-        {
-            // 降级：返回 IP(未知地区)，明确告知解析失败
-            return $"{ip}(未知地区)";
-        }
-    }
-
-    private string GetBrowserInfo(string userAgent)
-    {
-        if (string.IsNullOrWhiteSpace(userAgent))
-            return "Unknown";
-
-        try
-        {
-            var parser = Parser.GetDefault();
-            var info = parser.Parse(userAgent);
-
-            return $"{info.UA.Family} {info.UA.Major}.{info.UA.Minor}";
-        }
-        catch
-        {
-            return "Other";
-        }
     }
 }
---
+++ aspnet-core/services/TestWorkshop.HttpApi.Host/Extensions/IpLocationHelper.cs
@@ -0,0 +1,57 @@
+﻿using IPTools.Core;
+using UAParser;
+
+namespace TestWorkshop;
+
+/// <summary>
+/// IP归属地解析工具类
+/// </summary>
+public static class IpLocationHelper
+{
+    /// <summary>
+    /// 获取带归属地的IP地址
+    /// </summary>
+    /// <param name="ip">原始IP地址</param>
+    /// <returns>格式：IP(省份城市) 或 原始IP(未知地区)</returns>
+    public static string GetIpWithLocation(string ip)
+    {
+        if (string.IsNullOrEmpty(ip))
+            return ip;
+
+        // 处理本地地址
+        if (ip == "127.0.0.1" || ip == "::1" || ip == "0:0:0:0:0:0:0:1" || ip.Equals("localhost", StringComparison.OrdinalIgnoreCase))
+            return "127.0.0.1(本地)";
+
+        try
+        {
+            var info = IpTool.Search(ip);
+            return $"{info.IpAddress}({info.Province}{info.City})";
+        }
+        catch
+        {
+            return $"{ip}(未知地区)";
+        }
+    }
+
+    /// <summary>
+    /// 解析浏览器信息
+    /// </summary>
+    /// <param name="userAgent">User-Agent 字符串</param>
+    /// <returns>格式：浏览器名称 版本</returns>
+    public static string GetBrowserInfo(string userAgent)
+    {
+        if (string.IsNullOrWhiteSpace(userAgent))
+            return "Unknown";
+
+        try
+        {
+            var parser = Parser.GetDefault();
+            var info = parser.Parse(userAgent);
+            return $"{info.UA.Family} {info.UA.Major}.{info.UA.Minor}";
+        }
+        catch
+        {
+            return "Other";
+        }
+    }
+}
